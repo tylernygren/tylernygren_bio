feb0b4b3aecc666e98874bfdf9095a7899f46576
Tyler Nygren <tylernygren@gmail.com>
2021-08-18 10:55:47 -0400

    august

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+// app.use('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('main', { title: 'Home', icon: 'home.svg'})
-})
+    res.render('home', { title: 'Home', icon: 'home.svg'});
+});
 
 exports.app = functions.https.onRequest(app);
