ff41e754e2c3a47a7410cda76fc547f0a95647b3
Tyler Nygren <tylernygren@gmail.com>
2021-08-20 12:49:52 -0400

    august

--- server/index.js
+++ server/index.js
@@ -7,21 +7,7 @@ app.set('view engine', 'pug');
 // app.use('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Home', icon: 'home'});
-});
-
-app.post('/icons', (req, res) => {
-    let obj = {}; 
-    new Array(req.body).forEach(file => {
-        fs.readFile(__dirname + `/icons/${file}.svg`)
-            .then(data => {
-                obj[file] = { valid: true, string: data }
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    });
-    res.send(JSON.parse(obj));
+    res.render('home', { title: 'Home'});
 });
 
 exports.app = functions.https.onRequest(app);
